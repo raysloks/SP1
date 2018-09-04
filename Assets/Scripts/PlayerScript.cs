ce7e703194d8228129da4a744c15a64e435a0eb7
raysloks <raysloks@gmail.com>
2018-09-04 20:57:08 +0200

    added basic unity project

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.y - 1f);
+	}
+
+    private void FixedUpdate()
+    {
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical") * 0.5f) * Time.fixedDeltaTime);
+    }
+}
